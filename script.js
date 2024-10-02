4b6f2e2eec0cdc006f2688731c373f7263f1c20a
lanewells <wellsdelaneyk@gmail.com>
2024-10-01 22:13:14 -0700

    Declare first global variables

---
+++ script.js
@@ -0,0 +1,28 @@
+console.log('working')
+
+//global variables
+const magicNumber = document.querySelector('#magic-number')
+const rollDiceButton = document.querySelector('#roll-dice')
+const playAgainButton = document.querySelector('#play-again')
+const predictButton = document.querySelector('#predict')
+const predictionText = document.querySelector('#prediction-text')
+
+let inputElements = document.querySelectorAll('.input')
+let board = []
+let field = false
+let boardFull = false
+let number = 0
+
+
+//reset game function upon game loading
+
+
+//function: check if board full
+//function: roll dice
+//function: predict game
+//function: reset game
+
+//eventlistener: predict button - predict game
+//event listener: each input field - check if full
+//event listener: roll dice button - roll dice 
+//event listener: play again - reset game
